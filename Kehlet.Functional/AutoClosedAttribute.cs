e9e4f9ac331a038538bfb7488ce5fd67d5b8972d
Anders Kehlet <ake@kruso.dk>
2023-11-20 18:59:45 +0100

    disable warning

--- Kehlet.Functional/AutoClosedAttribute.cs
+++ Kehlet.Functional/AutoClosedAttribute.cs
@@ -1,4 +1,5 @@
-﻿namespace Kehlet.Functional;
+﻿#pragma warning disable CS9113 // Parameter is unread.
+namespace Kehlet.Functional;
 
 [AttributeUsage(AttributeTargets.Class)]
 public class AutoClosedAttribute(bool serializable = false) : Attribute
