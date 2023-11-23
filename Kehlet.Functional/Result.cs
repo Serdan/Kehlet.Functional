484426da778c65f6ee0215579090fb8812199cc0
Anders Kehlet <ake@kruso.dk>
2023-11-23 23:47:05 +0100

    x

--- Kehlet.Functional/Result.cs
+++ Kehlet.Functional/Result.cs
@@ -1,13 +1,24 @@
-﻿// ReSharper disable InconsistentNaming
+// ReSharper disable InconsistentNaming
 
 namespace Kehlet.Functional;
 
+/// <summary>
+/// Represents the outcome of a computation that can either succeed with a value or fail with an exception.
+/// </summary>
+/// <typeparam name="TValue">The type of the value returned in case of success.</typeparam>
 public readonly struct Result<TValue>
 {
     internal readonly TValue value;
     internal readonly Exception error;
 
+    /// <summary>
+    /// Gets a value indicating whether the result is successful.
+    /// </summary>
     public bool IsOk { get; }
+
+    /// <summary>
+    /// Gets a value indicating whether the result represents an error.
+    /// </summary>
     public bool IsError => !IsOk;
 
     private Result(TValue value)
@@ -24,26 +35,59 @@ public readonly struct Result<TValue>
         IsOk = false;
     }
 
+    /// <summary>
+    /// Applies one of the two provided functions based on the result state.
+    /// </summary>
+    /// <typeparam name="TResult">The type of the result returned by the provided functions.</typeparam>
+    /// <param name="ok">A function to apply when the result is successful.</param>
+    /// <param name="error">A function to apply when the result is an error.</param>
+    /// <returns>The result of applying the selected function.</returns>
     public TResult Match<TResult>(Func<TValue, TResult> ok, Func<Exception, TResult> error) =>
         IsOk
             ? ok(value)
             : error(this.error);
 
+    /// <summary>
+    /// Transforms the value of the result using a specified function if the result is successful.
+    /// </summary>
+    /// <typeparam name="TResult">The type of the result after applying the transformation function.</typeparam>
+    /// <param name="f">A transformation function to apply to the value.</param>
+    /// <returns>A new result object with the transformed value if the original result is successful, otherwise an error result.</returns>
     public Result<TResult> Select<TResult>(Func<TValue, TResult> f) =>
         IsOk
             ? ok(f(value))
             : error(error);
 
+    /// <summary>
+    /// Transforms the value of the result using a specified function that returns a result object if the original result is successful.
+    /// </summary>
+    /// <typeparam name="TResult">The type of the result returned by the transformation function.</typeparam>
+    /// <param name="f">A transformation function to apply to the value that returns a result object.</param>
+    /// <returns>A new result object from the transformation function if the original result is successful, otherwise an error result.</returns>
     public Result<TResult> Select<TResult>(Func<TValue, Result<TResult>> f) =>
         IsOk
             ? f(value)
             : error(error);
 
+    /// <summary>
+    /// Asynchronously transforms the value of the result using a specified function if the result is successful.
+    /// </summary>
+    /// <typeparam name="TResult">The type of the result after applying the transformation function.</typeparam>
+    /// <param name="f">An asynchronous transformation function to apply to the value.</param>
+    /// <returns>A task representing the asynchronous operation, containing a new result object with the transformed value if the original result is successful, otherwise an error result.</returns>
     public async Task<Result<TResult>> Select<TResult>(Func<TValue, Task<TResult>> f) =>
         IsOk
             ? ok(await f(value))
             : error(error);
 
+    /// <summary>
+    /// Projects the value of the result to a new form using a specified selector function, then transforms the result of that function using another result selector function.
+    /// </summary>
+    /// <typeparam name="TValue2">The type of the intermediate result returned by the selector function.</typeparam>
+    /// <typeparam name="TResult">The type of the final result returned by the result selector function.</typeparam>
+    /// <param name="selector">A function to transform the original value to an intermediate result.</param>
+    /// <param name="resultSelector">A function to transform the intermediate result to the final result.</param>
+    /// <returns>A new result object representing the transformed value.</returns>
     public Result<TResult> SelectMany<TValue2, TResult>(Func<TValue, Result<TValue2>> selector, Func<TValue, TValue2, TResult> resultSelector)
     {
         if (IsError)
@@ -61,6 +105,14 @@ public readonly struct Result<TValue>
         return ok(resultSelector(value, inner.value));
     }
 
+    /// <summary>
+    /// Projects the value of the result to a new form using a specified selector function, then transforms the result of that function using another function that returns a new result object.
+    /// </summary>
+    /// <typeparam name="TValue2">The type of the intermediate result returned by the selector function.</typeparam>
+    /// <typeparam name="TResult">The type of the final result returned by the result selector function.</typeparam>
+    /// <param name="selector">A function to transform the original value to an intermediate result.</param>
+    /// <param name="resultSelector">A function to transform the intermediate result to a final result object.</param>
+    /// <returns>A new result object representing the final transformed value.</returns>
     public Result<TResult> SelectMany<TValue2, TResult>(Func<TValue, Result<TValue2>> selector, Func<TValue, TValue2, Result<TResult>> resultSelector)
     {
         if (IsError)
@@ -78,28 +130,89 @@ public readonly struct Result<TValue>
         return resultSelector(value, inner.value);
     }
 
+    /// <summary>
+    /// Returns the value if the result is successful, or applies a specified function to the error if it's not.
+    /// </summary>
+    /// <param name="f">A function to apply to the error when the result is not successful.</param>
+    /// <returns>The original value if the result is successful; otherwise, the result of the function applied to the error.</returns>
     public TValue IfError(Func<Exception, TValue> f) =>
         IsOk
             ? value
             : f(error);
 
-    public Result<TValue> SelectError(Func<Exception, Exception> f) =>
+    /// <summary>
+    /// Transforms the error of the result using a specified function if the result is not successful.
+    /// </summary>
+    /// <param name="f">A transformation function to apply to the error.</param>
+    /// <returns>A new result object with the transformed error if the original result is not successful, otherwise the original result.</returns>
+    public Result<TValue> SelectError(Func<Exception, Exception> f)
+
+    /// <summary>
+    /// Transforms the error of the result using a specified function if the result is not successful, otherwise returns the original result.
+    /// </summary>
+    /// <typeparam name="TResult">The type of the result returned by the alternative function.</typeparam>
+    /// <param name="alternativeFactory">A function to apply when the result is an error, returning a new result object.</param>
+    /// <returns>A new result object if the original result is an error; otherwise, the original result.</returns>
+    public Result<TResult> OrElse<TResult>(Func<Exception, Result<TResult>> alternativeFactory)
+    {
+        return IsError ? alternativeFactory(error) : this;
+    }
+ =>
         IsOk
             ? this
             : error(f(error));
 
+    /// <summary>
+    /// Returns a string representation of the result, showing a successful value or the error message.
+    /// </summary>
+    /// <returns>A string representation of the result.</returns>
     public override string ToString() =>
         IsOk
             ? $"ok {value}"
             : $"error {error.Message}";
 
+    /// <summary>
+    /// Creates a successful result object with the provided value.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the value to be included in the result.</typeparam>
+    /// <param name="value">The value to include in the result.</param>
+    /// <returns>A successful result object containing the provided value.</returns>
     public static Result<TValue> OkResult(TValue value) => new(value);
+
+    /// <summary>
+    /// Creates an error result object with the provided exception.
+    /// </summary>
+    /// <param name="exception">The exception to include in the error result.</param>
+    /// <returns>An error result object containing the provided exception.</returns>
     public static Result<TValue> ErrorResult(Exception exception) => new(exception);
 
+    /// <summary>
+    /// Implicitly converts an ErrorResult to a Result object, encapsulating the provided exception.
+    /// </summary>
+    /// <param name="errorResult">The ErrorResult to convert.</param>
+    /// <returns>A Result object representing the error.</returns>
     public static implicit operator Result<TValue>(ErrorResult errorResult) => ErrorResult(errorResult.Exception);
+
+    /// <summary>
+    /// Implicitly converts a ResultUnion.Ok to a Result object, encapsulating the successful value.
+    /// </summary>
+    /// <param name="result">The ResultUnion.Ok to convert.</param>
+    /// <returns>A Result object representing the successful value.</returns>
     public static implicit operator Result<TValue>(ResultUnion<TValue>.Ok result) => OkResult(result.Value);
+
+    /// <summary>
+    /// Implicitly converts a ResultUnion.Error to a Result object, encapsulating the provided exception.
+    /// </summary>
+    /// <param name="result">The ResultUnion.Error to convert.</param>
+    /// <returns>A Result object representing the error.</returns>
     public static implicit operator Result<TValue>(ResultUnion<TValue>.Error result) => ErrorResult(result.Exception);
 
+    /// <summary>
+    /// Defines a custom operator | to select the first successful result between two results.
+    /// </summary>
+    /// <param name="lhs">The first result to evaluate.</param>
+    /// <param name="rhs">The second result to evaluate.</param>
+    /// <returns>The first successful result if either is successful, otherwise the second result.</returns>
     public static Result<TValue> operator |(Result<TValue> lhs, Result<TValue> rhs) => lhs.IsOk ? lhs : rhs;
 }
 
