044194b4f34f34ba0dbd0eebcc44464c0fa90c95
Anders Kehlet <ake@kruso.dk>
2023-12-30 21:59:07 +0100

    effect target

---
+++ Kehlet.Functional/AsyncEffect/AsyncEffectTarget.cs
@@ -0,0 +1,32 @@
+﻿namespace Kehlet.Functional;
+
+[UsedImplicitly]
+public readonly ref struct AsyncEffectTarget<TValue>
+    where TValue : notnull
+{
+    public static AsyncEffect<TValue> operator <<(AsyncEffectTarget<TValue> _, Func<Task<Result<TValue>>> f) =>
+        new(f);
+
+    public static AsyncEffect<TValue> operator <<(AsyncEffectTarget<TValue> _, Func<AsyncEffect<TValue>> f) =>
+        new(() => f().Run());
+
+    public static AsyncEffect<TValue> operator <<(AsyncEffectTarget<TValue> _, Task<Result<TValue>> result) =>
+        new(() => result);
+
+    public static AsyncEffect<TValue> operator <<(AsyncEffectTarget<TValue> _, AsyncEffect<TValue> effect) =>
+        new(effect.Run);
+}
+
+[UsedImplicitly]
+public readonly ref struct AsyncEffectTarget<TRuntime, TValue>
+    where TValue : notnull
+{
+    public static AsyncEffect<TRuntime, TValue> operator <<(AsyncEffectTarget<TRuntime, TValue> _, Func<TRuntime, Task<Result<TValue>>> f) =>
+        new(f);
+
+    public static AsyncEffect<TRuntime, TValue> operator <<(AsyncEffectTarget<TRuntime, TValue> _, Func<TRuntime, AsyncEffect<TRuntime, TValue>> f) =>
+        new(runtime => f(runtime).Run(runtime));
+
+    public static AsyncEffect<TRuntime, TValue> operator <<(AsyncEffectTarget<TRuntime, TValue> _, AsyncEffect<TRuntime, TValue> effect) =>
+        new(effect.Run);
+}
---
+++ Kehlet.Functional/Effect/EffectTarget.cs
@@ -0,0 +1,32 @@
+﻿namespace Kehlet.Functional;
+
+[UsedImplicitly]
+public readonly ref struct EffectTarget<TValue>
+    where TValue : notnull
+{
+    public static Effect<TValue> operator <<(EffectTarget<TValue> _, Func<Result<TValue>> f) =>
+        new(f);
+
+    public static Effect<TValue> operator <<(EffectTarget<TValue> _, Func<Effect<TValue>> f) =>
+        new(() => f().Run());
+
+    public static Effect<TValue> operator <<(EffectTarget<TValue> _, Result<TValue> result) =>
+        new(() => result);
+
+    public static Effect<TValue> operator <<(EffectTarget<TValue> _, Effect<TValue> effect) =>
+        new(effect.Run);
+}
+
+[UsedImplicitly]
+public readonly ref struct EffectTarget<TRuntime, TValue>
+    where TValue : notnull
+{
+    public static Effect<TRuntime, TValue> operator <<(EffectTarget<TRuntime, TValue> _, Func<TRuntime, Result<TValue>> f) =>
+        new(f);
+
+    public static Effect<TRuntime, TValue> operator <<(EffectTarget<TRuntime, TValue> _, Func<TRuntime, Effect<TRuntime, TValue>> f) =>
+        new(runtime => f(runtime).Run(runtime));
+
+    public static Effect<TRuntime, TValue> operator <<(EffectTarget<TRuntime, TValue> _, Effect<TRuntime, TValue> effect) =>
+        new(effect.Run);
+}
