c91f4c84b9ef7ba104e23c5991ca3e84a24a5feb
Anders Kehlet <ake@kruso.dk>
2024-01-01 16:20:01 +0100

    console trait

---
+++ Kehlet.Functional/Environment/IConsoleIO.cs
@@ -0,0 +1,16 @@
+﻿using Kehlet.Generators;
+
+namespace Kehlet.Functional;
+
+[FromStaticMembers(typeof(Console), true, typeof(Unit))]
+public partial interface IConsoleIO;
+
+[UsedImplicitly]
+public readonly struct ConsoleIO : IConsoleIO;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public interface IHasConsole<TRuntime>
+    where TRuntime : struct, IHasConsole<TRuntime>
+{
+    Effect<TRuntime, IConsoleIO> Console { get; }
+}
