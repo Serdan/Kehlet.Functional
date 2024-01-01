5e58020b584713a0ff6a942e9285a6ce1d0b8942
Anders Kehlet <ake@kruso.dk>
2024-01-02 00:10:29 +0100

    process trait

---
+++ Kehlet.Functional/Environment/IProcessIO.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using Kehlet.Generators;
+
+namespace Kehlet.Functional;
+
+[FromInstanceMembers(typeof(Process), implement: true, typeof(Unit))]
+public partial interface IProcessIO;
+
+public readonly struct ProcessIO(Process process) : IProcessIO
+{
+    public Process Instance { get; } = process;
+
+    public override string ToString() =>
+        Instance.ToString();
+}
+
+public interface IHasProcessIO<TRuntime>
+    where TRuntime : struct, IHasProcessIO<TRuntime>
+{
+    Effect<IProcessIO> ProcessIO(ProcessStartInfo info) =>
+        Process.Start(info) is { } process
+            ? okEffect((IProcessIO) new ProcessIO(process))
+            : errorEffect<IProcessIO>("Failed to start process.");
+}
